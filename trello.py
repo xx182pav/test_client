064f5a59266c41ded089810134b6e7117a0735f0
xx182pav <53460770+xx182pav@users.noreply.github.com>
2019-11-19 17:04:35 +0300

    Update trello.py

--- trello.py
+++ trello.py
@@ -1,145 +1,105 @@
-import sys 
-import requests    
+import requests
+import datetime
 
-api_key = ""
-token = ""
+# Данные авторизации в API Trello
+auth_params = {
+    'key': "",
+    'token': "", }
 
-base_url = "https://api.trello.com/1/{}" 
-auth_params = {    
-    'key': api_key,    
-    'token': token, }
-board_id = ""    
-    
-def read():   
-    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
-  
+# Адрес, на котором расположен API Trello, # Именно туда мы будем отправлять HTTP запросы.
+base_url = "https://api.trello.com/1/{}"
+board_id = ""
+
+
+def read():
+    # Получим данные всех колонок на доске:
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    #    Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:
     for column in column_data:
-        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()       
-        if len(task_data) == 1:
-            print(column['name'], "[Currently 1 item]")
-        else:
-            print(column['name'], f"[Currently {len(task_data)} items]")   
-
-        if not task_data:      
-            print('\t' + 'No tasks found')      
-            continue      
+        print(column['name'])
+        # Получим данные всех задач в колонке и перечислим все названия
+        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        print('Количество задач:', len(task_data))
+        if not task_data:
+            print('\t' + 'Нет задач!')
+            continue
         for task in task_data:
-            print('\t' + task['name'], f"[{task['id']}]")   
-    
-def create(name, column_name): 
-    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
+            print('\t' + task['name'])
 
+
+def create():
+    name = input("Введите название новой задачи\n")
+    column_name = input("Введите название колонки, куда поместить создаваемую задачу\n")
+    # Получим данные всех колонок на доске
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна
     for column in column_data:
         if column['name'] == column_name:
-            requests.post(base_url.format('cards'), params={'name': name, 'idList': column['id'], **auth_params})      
+            # Создадим задачу с именем _name_ в найденной колонке
+            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
             break
 
-    
-def move(name, column_name):       
-    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()    
-        
-            
-    for column in column_data:    
-        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json() 
-        tasks = {}   
-        for task in column_tasks:
-            if task['name'] in tasks.keys():
-                tasks[task['name']].append(task['id'])
-            else:
-                tasks[task['name']] = [task['id']]
 
-        if name not in tasks.keys():
-                continue
-        else:
-            if len(tasks[name]) > 1:
-                for i in range(len(tasks[name])):
-                    print(f"\t {i+1} {tasks[name][i]}")
-                task_id = tasks[name][int(input(f"There are more than 1 occurence of the task '{name}'. Which one do you want to move? \n"))-1]
-                break
-            task_id = tasks[name][0]
-
-    columns = {}
+def move():
+    name = input("Введите название задачи, которую хотите переместить\n")
+    # Получим данные всех колонок на доске
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    # Среди всех колонок нужно найти задачу по имени и получить её id
+    task_move = ''
+    task_id = []
+    col_with_tasks = []
+    timestamps = []
     for column in column_data:
-
-        if column['name'] in columns.keys():
-            columns[column['name']].append(column['id'])
-        else:
-            columns[column['name']] = [column['id']]
-    if len(columns[column_name]) > 1:
-        for i in range(len(columns[column_name])):
-                    print(f"\t {i+1} {columns[column_name][i]}")
-        column_id = columns[column_name][int(input(f"There are more than 1 occurence of the column '{column_name}'. Which one do you want the task to be moved to? \n"))-1]
-
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        for task in column_tasks:
+            if task['name'] == name:
+                task_id.append(task['id'])
+                col_with_tasks.append(column['name'])
+                timestamps.append(
+                    datetime.datetime.strptime(task['dateLastActivity'].split(".")[0], "%Y-%m-%dT%H:%M:%S"))
+    if len(task_id) > 1:
+        print("Задач с таким именем несколько:")
+        for i in range(len(task_id)):
+            print(f"{i + 1}: из колонки {col_with_tasks[i]}, последнее изменение: {timestamps[i]}")
+        target_task = int(input("Введите номер задачи, которую нужно переместить\n")) - 1
+        task_move = task_id[target_task]
     else:
-        column_id = columns[column_name][0]
+        task_move = task_id[0]
+    # Теперь, когда у нас есть id задачи, которую мы хотим переместить
+    # Переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу    
+    column_name = input("Введите название колонки, куда поместить задачу\n")
+    for column in column_data:
+        if column['name'] == column_name:
+            # И выполним запрос к API для перемещения задачи в нужную колонку
+            requests.put(base_url.format('cards') + '/' + task_move + '/idList',
+                         data={'value': column['id'], **auth_params})
 
-    requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column_id, **auth_params})       
+
+def add_column():
+    name = input("Введите название новой колонки\n")
+    pos = input("Хотите поместить новую колонку в начало или конец?"
+                "\n Варианты ввода: 'top' и 'bottom'"
+                "\n Если ничего не вводить, то по умолчанию будет 'в конец'\n")
+    if not pos:
+        pos = 'bottom'
+    requests.post(base_url.format('boards') + '/' + board_id + '/lists', data={'name': name, 'pos': pos, **auth_params})
 
 
-def create_column(column_name, name=""):
-    requests.post(base_url.format('boards') + '/' + board_id + '/lists', params={'name': column_name, "pos": "bottom", **auth_params})
-    if name:
-    	create(name, column_name)
+def main_interface():
+    print("Добро пожаловать в интерфейс программы по работе с сервером Trello через консоль ")
+    choice = input("Нажмите 1, если хотите увидеть список задач,"
+                   "\nнажмите 2, чтобы добавить новую колонку,"
+                   "\nнажмите 3, чтобы переместить задачу из одной колонки в другую."
+                   "\nнажмите 4, чтобы создать новую задачу\n")
+    if choice == "1":
+        read()
+    elif choice == "2":
+        add_column()
+    elif choice == "3":
+        move()
+    elif choice == "4":
+        create()
+
 
 if __name__ == "__main__":
-    while True:
-        action = input("Choose an action: \ntype 'read' to see the list of list and cards\ntype 'move' to move a card from one list to another\ntype 'create' to create a new card\ntype 'create_column' to create a new list\ntype 'break' to quit\n")    
-        if action == 'read':
-            read() 
-
-        elif action == 'create':
-            while True:
-                try:
-                    name = input("Give a name to your card: ")
-                    column_name = input("Type the name of the list where your card will be created: ")
-                    create(name, column_name)
-                    print("Success")
-                    break
-                except KeyboardInterrupt:
-                    break
-                except:
-                    print("Incorrect values. Use CTRL+C to exit, or continue\n")
-
-        elif action == 'move':    
-            while True:
-                try:
-                    name = input("Type the name of the card you want to move: ")
-                    column_name = input("Type the name of the list where your card will be moved: ")
-                    move(name, column_name)
-                    print("Success")
-                    break
-                except KeyboardInterrupt:
-                    break
-                except:
-                    print("Incorrect values. Use CTRL+C to exit, or continue\n")
-
-        elif action == 'create_column':
-            while True:
-                try:
-                    column_name = input("Give a name to your list: ")
-                    resp = input("Do you want do add a task to your list? If no, your list will be empty. Y/N: ")
-                    try:
-                        if resp.lower() == 'y':
-                            name = input('Give your new task a name: ')
-                            create_column(column_name, name)
-                            print("Success")
-                            break
-                        elif resp.lower() == 'n':
-                            create_column(column_name)
-                            print('Success')
-                            break
-                    except KeyboardInterrupt:
-                        break
-                    except:
-                        print("Incorrect values. Use CTRL+C to exit, or continue\n")
-
-                    
-                    break
-                except KeyboardInterrupt:
-                    break
-                except:
-                    print("Incorrect values. Use CTRL+C to exit, or continue\n")
-
-        elif action == 'break':
-            print('Terminating...')
-            break
+    main_interface()
